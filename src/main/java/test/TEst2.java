0916b06ce3e59d94d087a061277bf1c8ea326cb5
pedrodelg <pedro7jd@gmail.com>
2020-04-03 18:14:00 -0500

    initial commit

---
+++ src/main/java/test/TEst2.java
@@ -0,0 +1,9 @@
+package test;
+
+public class TEst2 {
+
+	public static int testMethod() {
+		
+		return 2;
+	}
+}
---
+++ src/test/java/com/luv2code/sprinboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.luv2code.sprinboot.cruddemo;
+
+
+
+import org.junit.jupiter.api.Test;
+
+
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		
+		
+	}
+
+}
